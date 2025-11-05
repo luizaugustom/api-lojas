d03800019e77096716f39f0f804a4cd3575ee89a
luizaugustom <luizlamdv@gmail.com>
2025-11-05 19:20:28 -0300

    sem impressão

--- src/application/printer/printer.module.ts
+++ src/application/printer/printer.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
-// import { ScheduleModule } from '@nestjs/schedule'; // Removido - Cron de verificação automática desabilitado
 import { PrinterService } from './printer.service';
-import { PrinterController } from './printer.controller';
 import { PrismaModule } from '../../infrastructure/database/prisma.module';
-import { PrinterDriverService } from '../../shared/services/printer-driver.service';
 import { ThermalPrinterService } from '../../shared/services/thermal-printer.service';
 
 @Module({
-  imports: [
-    PrismaModule,
-    // ScheduleModule.forRoot(), // Removido - Cron de verificação automática desabilitado
-  ],
-  providers: [
-    PrinterService,
-    PrinterDriverService,
-    ThermalPrinterService,
-  ],
-  controllers: [PrinterController],
+  imports: [PrismaModule],
+  providers: [PrinterService, ThermalPrinterService],
   exports: [PrinterService],
 })
 export class PrinterModule {}
+
